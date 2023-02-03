317de5dcc173fab36b03244f3b43d76be1194087
kerim-kilic <mail@kerim-kilic.com>
2023-02-03 12:03:34 +0100

    Clean unused libraries

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
 from pyspark.sql import *
-from pyspark.ml.classification import *
-from pyspark.ml.evaluation import BinaryClassificationEvaluator
-from pyspark.ml.linalg import Vectors
-from pyspark.ml.tuning import CrossValidator, ParamGridBuilder, CrossValidatorModel
-from pyspark.sql.functions import *
-from pyspark.ml.feature import *
-from pyspark.pandas import *
 from src.data_pipeline import *
 from src.data_split import *
-
-from pysparkling import *
-import h2o
 
 # Create Spark session
 spark = SparkSession.builder \
@@ -18,12 +8,10 @@ spark = SparkSession.builder \
       .appName("test") \
       .getOrCreate()
 
-# h2oContext = H2OContext.getOrCreate(spark)
-
 ### Read in the data
-raw_data = spark.read.csv("data/flights_2017.csv", header=True)
-origin_weather_data = spark.read.csv("data/origin_weather_data.csv", header=True)
-destination_weather_data = spark.read.csv("data/destination_weather_data.csv", header=True)
+raw_data = spark.read.csv("/home/data/flights_2017.csv", header=True)
+origin_weather_data = spark.read.csv("/home/data/origin_weather_data.csv", header=True)
+destination_weather_data = spark.read.csv("/home/data/destination_weather_data.csv", header=True)
 
 df = process_data(raw_data, origin_weather_data, destination_weather_data)
 
@@ -36,3 +24,4 @@ train_data = splits[0]
 test_data = splits[1]
 
 spark.stop()
+print("Finished running script.")
