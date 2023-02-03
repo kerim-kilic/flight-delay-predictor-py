0148c96f9d49e5080957d665b55e3e0c4c66d790
kerim-kilic <mail@kerim-kilic.com>
2023-02-03 14:55:13 +0100

    Fix bash script to use working directory instead

--- run_script.sh
+++ run_script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker image build -t flight-delay-prediction-python:scripted_build .
-docker container run -it -v ~/PycharmProjects/flight-delay-prediction-python/data/:/home/data/ -v ~/PycharmProjects/flight-delay-prediction-python/src/:/home/src/ --name container flight-delay-prediction-python:scripted_build
+docker container run -it -v $(pwd)/data/:/home/data/ -v $(pwd)/src/:/home/src/ --name container flight-delay-prediction-python:scripted_build
 docker container rm container
